6b41fbf3fd0676fd4be5992abedcf812ba01dab3
azermu <1390973247@qq.com>
2021-04-01 22:30:37 +0800

    delete SSM-heima-cast-ssm

--- SSM-heima-cast-ssm/src/main/resources/account.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `account` (
-  `id` int(32) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `money` double NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
